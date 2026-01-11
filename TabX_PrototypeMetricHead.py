c7492a9124aff1047796b7e22eac711bcb761371
AnhND <anh@gmail.com>
2026-01-11 21:04:51 +0700

    Update

--- TabX_PrototypeMetricHead.py
+++ TabX_PrototypeMetricHead.py
@@ -304,7 +304,7 @@ def train_rnn_extractor(model, train_loader, val_loader, criterion, optimizer, e
             auc = roc_auc_score(all_lbls, all_preds)
             aupr = average_precision_score(all_lbls, all_preds)
             auc_val = 2 * auc * aupr / (auc + aupr + 1e-8)  # Fused AUC Metric
-
+            auc_val = aupr  # Use AUPR for early stopping
             print(f"    Epoch {epoch+1} Val AUC: {auc_val:.4f}")
             
             if auc_val > best_auc:
